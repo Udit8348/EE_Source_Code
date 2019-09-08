ed1d0adfbe89cba0a4c12f6c1188a182728fc072
Udit Subramanya <udit610@yahoo.com>
2019-09-07 20:09:24 -0400

    Add files via upload

--- opcontrol.cpp
+++ opcontrol.cpp
@@ -2,8 +2,8 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-
 using namespace pros;
+
 int a, b = 0;
 int count = 0;
 bool bHasSettled = false;
@@ -11,148 +11,121 @@ int independent = 0;
 int trialNumber = 0;
 int startTime = 0;
 std::ofstream outputFILE;
-std::string outputDIR = "ee_data_9_6_19_10__31";	//file directory
-
+std::string outputDIR = "ee_data_sept_7_05_";	//file directory
 std::string outputPATH = "/usd/" + outputDIR + std::to_string(independent) + "_" + std::to_string(trialNumber) + ".csv";
 std::string outputDATA = "";
 std::string outputTIME = "";
 ADIEncoder encoder (1, 2, false);
 ADIDigitalIn startBtn (3);
 
-void redoCollection() {
-	trialNumber = 0;
-}
-
-void incrementIndepentVariable(){
-	independent++;
-	trialNumber = 0;
+void resetEncoder(){
 	encoder.reset();
-	a = 0;
-	b = 0;
-	outputDATA = "";
-	outputTIME = "";
-	outputPATH = "/usd/" + outputDIR + std::to_string(independent) + "_" + std::to_string(trialNumber) + ".csv";
-
 }
 
 void saveTrial(){
 	lcd::clear_line(0);
 	lcd::clear_line(1);
-	lcd::set_text(0, "Test Saved");
+	//Update path
+	outputPATH = "/usd/" + outputDIR + std::to_string(independent) + "_" + std::to_string(trialNumber) + ".csv";
 	outputFILE.open (outputPATH);
-	outputFILE << outputDATA + "\n";
-	outputFILE << outputTIME + "\n";
+	outputFILE << outputTIME + "\n";	//x-axis data
+	outputFILE << outputDATA + "\n";	//y-aixs data
 	outputFILE.close();
+	lcd::set_text(0, "Test Saved");
 }
 
 void viewLiveValues(void* param){
+	encoder.reset();
 	while(1){
+		//Always display all sensor values
 		lcd::print(6,"%d", encoder.get_value());
 		lcd::print(7,"%d", startBtn.get_value());
 
-		if(encoder.get_value() < -40){
-				lcd::print(3, "%s", outputDATA);
-				lcd::print(4, "%s", outputTIME);
-				lcd::print(5, "%s", outputPATH);
-			}else{
-				lcd::clear_line(3);
-				lcd::clear_line(4);
-				lcd::clear_line(5);
-
-			}
-
+		//Display all output information when the arm is pushed forward 90 degrees
+		if(encoder.get_value() < -90){
+			lcd::print(3, "%s", outputDATA);
+			lcd::print(4, "%s", outputTIME);
+			lcd::print(5, "%s", outputPATH);
+		}else{
+			lcd::clear_line(3); lcd::clear_line(4); lcd::clear_line(5);
+		}
 		delay(10);
 	}
 }
 
 
 void opcontrol() {
-	lcd::register_btn0_cb(incrementIndepentVariable);	//left button
-	lcd::register_btn1_cb(redoCollection);	//middle button
-	lcd::register_btn2_cb(saveTrial);
-	encoder.reset();
+	lcd::register_btn2_cb(saveTrial);	//right button
+	lcd::register_btn0_cb(resetEncoder);
 	Task debug(viewLiveValues, NULL);
+	delay(50);
 
+	//Does Test Prodecure 5 Times for 5 Different Masses
+	for (int j; j < 5; j++){
+		//keep track of independent variable
+		independent = j;
+	for (int i = 0; i < 5; i++) {
+		//keep track of trial number
+		trialNumber = i;
 
-	while (true) {
-	/*
-if(startBtn.get_value()){
-			if(!b){
-				outputTIME+= std::to_string(millis());
-				b = 1;
-			}else{
-				outputTIME+= "," + std::to_string(millis());
-			}
-
-			if(!a){
-				outputDATA += std::to_string(1);
-				a = 1;
-			}else{
-				outputDATA += "," + std::to_string(1);
-			}
-		}
-*/
-// Test Prodecure starts here
+		//Wait for arm to be pulled back to the button
 		while(startBtn.get_value() == 0){
 			lcd::set_text(0, "Pull Back Arm");
 			delay(2);
 		}
 
-		//reset test variables
-		a = 0;
-		b = 0;
-		count = 0;
-		bHasSettled = false;
+		//Resets test variables
+		a = 0; b = 0; count = 0; bHasSettled = false;
 
+		//Waits for arm to be released from the button
 		while(startBtn.get_value()){
 			lcd::set_text(0, "Release Arm");
 			delay(2);
 		}
-
 		startTime = millis();
 
+		//Completes data collection until the arm has settled
 		while(!bHasSettled){
-
+			lcd::set_text(0, "Testing...");
 			//Log time
-			if(!b){
-				outputTIME+= std::to_string(millis() - startTime);
-				b = 1;
+			if(!a){
+				outputTIME+= std::to_string(millis() - startTime); a = 1;
 			}else{
 				outputTIME+= "," + std::to_string(millis() - startTime);
 			}
 
 			//log data
-			if(!a){
-				outputDATA += std::to_string(encoder.get_value());
-				a = 1;
+			if(!b){
+				outputDATA += std::to_string(encoder.get_value()); b = 1;
 			}else{
 				outputDATA += "," + std::to_string(encoder.get_value());
 			}
 
-			//check if settled
-			if(abs(encoder.get_value()) < 3){
+			//counts how long the arm remains within the accepted settled zone
+			if(abs(encoder.get_value()) < 9){
 				count++;
 			}else{
 				count = 0;
 			}
 
-			//complete test when its considered settled (% or just a const?)
+			//determines that the test is complete when it has remained settled for 10 counts
 			if(count > 10){
 				bHasSettled = true;
 			}
+			//samples data every 50 ms (20 Hz)
 			delay(50);
 		}
 
-		lcd::set_text(0, "Test Complete");
-		lcd::set_text(1, "Please Save");	//save trial by pressing right button
-
-		//reset position to where it has stopped
 		encoder.reset();
+		lcd::print(0, "IDV %d, Trial %d Complete", j+1, i + 1);
+		delay(500);
+		saveTrial();
+	}//end of trials for loop
 
-		while(1){
-			delay(2);
-		}
+	lcd::print(0, "All IDV %d Trials Complete", independent + 1);
+	delay(2000);
+}//end of independent variable for loop
 
-		delay(10);	// 10ms delay
-	}
+//lcd::clear();
+lcd::set_text(0, "TESTING COMPLETE :)");
 }
